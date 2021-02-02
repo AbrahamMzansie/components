46618693773ace6c9b691ce467319e752e6884c9
Abraham Nkomo <abraham.nkomo@omnistall.com>
2021-02-02 09:40:44 +0200

    Added a user post component

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src="" />
+        </a>
+        <div className="content">
+          <a href="/" className="Author">
+            Abraham
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6 : 00PM</span>
+          </div>
+          <div className="Text">
+            Steven Grider is giving the tutorial on React.JS{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
